17ad1a4c1c5466fa1e7d7223258178ae1a6142e5
mariosrb <marrio-2000@gotmail.fr>
2024-11-26 17:40:54 +0100

    dernier

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: mdodevsk <mdodevsk@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/26 15:08:56 by mdodevsk          #+#    #+#             */
-/*   Updated: 2024/11/26 16:55:47 by mdodevsk         ###   ########.fr       */
+/*   Updated: 2024/11/26 17:27:51 by mdodevsk         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,13 +14,18 @@
 
 char	*get_next_line(int fd)
 {
-	
+	char	*line;
+	static char		*buffer;
+
+	return (line);
 }
 
 int	main(void)
 {
 	char	buffer_bytes[BUFFER_SIZE];
 	char	*buffer;
-	
+	int		fd;
+
+	fd = open("fichier.txt", O_RDONLY);
 	return (0);
 }
