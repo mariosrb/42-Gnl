66660d6d0e49f77e9879b28daa3d9d660c7f7e2a
mariosrb <mario-2000@hotmail.fr>
2024-11-29 17:42:23 +0100

    test

--- exercice.c
+++ exercice.c
@@ -16,39 +16,90 @@
 		return ((char *)s);
 	return (NULL);
 }*/
+// char	*check_buffer(char *buffer)
+// {
+// 	int	i;
+// 	char	*res;
 
-char	*gnl(int fd)
-{
-	char	*cache = NULL;
-	static char	*temp;
-	char	*buffer = NULL;
-	int		nb_read;
-	char	*new_temp;
 
-	buffer = malloc(sizeof(char) * BUFFER_SIZE + 1);
+// 	i = 0; // bonjour je suis/n mario et un chat 
+// 	while (buffer[i])
+// 	{
+// 		if (buffer[i] == '\n')
+// 			{
+
+// 			}
+// 		i++;
+// 	}
+// }
+
+// char	*gnl(int fd)
+// {
+// 	char	*cache = NULL;
+// 	static char	*temp;
+// 	char	*buffer = NULL;
+// 	int		nb_read;
+// 	char	*new_temp;
+
+// 	buffer = malloc(sizeof(char) * BUFFER_SIZE + 1);
 	
-	nb_read = read(fd, buffer, BUFFER_SIZE);
-	while (nb_read > 0)
+// 	nb_read = read(fd, buffer, BUFFER_SIZE);
+// 	while (nb_read > 0)
+// 	{
+// 		temp = ft_strdup(buffer);
+// 		if (!cache)
+// 			cache = ft_strdup(temp);
+// 		else
+// 		{
+// 			cache = ft_strjoin(cache, temp);
+// 		}
+// 		nb_read = read(fd, buffer, BUFFER_SIZE);
+// 		buffer[nb_read] = '\0';
+// 	}
+// 	return (cache);
+// }
+
+char	*extract_line(char *input)
+{
+	static char	*temp;
+	char		*res;
+	int			i;
+	int			j;
+
+	i = 0;
+	j = 0;
+	while (input[i] != '\n' && input[i])
+		i++;
+	res = malloc(sizeof(char) * (i + 2));
+	if (!res)
+		return (NULL);
+	while (j < i)
 	{
-		temp = ft_strdup(buffer);
-		if (!cache)
-			cache = ft_strdup(temp);
-		else
-		{
-			cache = ft_strjoin(cache, temp);
-		}
-		nb_read = read(fd, buffer, BUFFER_SIZE);
-		buffer[nb_read] = '\0';
+		res[j] = input[j];
+		j++;
 	}
-	return (cache);
+	if (input[i] == '\n')
+	 	res[i++] = '\n';
+	res[j] = '\0';
+	if (input[i])
+	{
+	 	free(temp);
+		temp = ft_strdup(&input[i]);
+	}
+	else
+		{
+			free(temp);
+			temp = NULL;
+		}
+	//printf("%s\n", temp);
+	return (res);
 }
-
-
 
 int	main(int ac, char **av)
 {
-	int fd = open(av[1], O_RDONLY);
-	printf("%s\n", gnl(fd));
-	close (fd);
+	printf("%s\n", extract_line("Bonjour\nje suis un chat"));
+	// int fd = open(av[1], O_RDONLY);
+	// printf("%s\n", gnl(fd));
+	// close (fd);
 	return (0);
 }
--- get_next_line.c
+++ get_next_line.c
@@ -6,13 +6,18 @@
 /*   By: mdodevsk <mdodevsk@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/26 15:08:56 by mdodevsk          #+#    #+#             */
-/*   Updated: 2024/11/28 21:28:43 by mdodevsk         ###   ########.fr       */
+/*   Updated: 2024/11/29 15:34:41 by mdodevsk         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-/*char	*get_next_line(int fd)
+char	*read_buffer(int fd, char *res)
+{
+	
+}
+
+char	*get_next_line(int fd)
 {
 	char	*res;
 	static char		*buffer;
@@ -23,5 +28,8 @@
 
 int	main(int ac, char **av)
 {
-	
-}*/
+	int fd = open(av[1], O_RDONLY);
+	printf("%s\n", gnl(fd));
+	close (fd);
+	return (0);
+}
